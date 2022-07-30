6be70970bf08628ca16456f083d88b55e93d1dcd
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2022-07-30 21:11:37 +0100

    feat: initial tourist number page structure

---
+++ src/components/touristNumber/TouristNumberTitle.tsx
@@ -0,0 +1,11 @@
+import "./TouristNumberTitle.css";
+
+const TouristNumberTitle: React.FC = () => {
+  return (
+    <div className="tourist-number-nationalities-container">
+      <h1>Número de turistas</h1>
+    </div>
+  );
+};
+
+export default TouristNumberTitle;
--- src/pages/TouristNumber.tsx
+++ src/pages/TouristNumber.tsx
@@ -1,5 +1,20 @@
+import { IonContent, IonHeader, IonPage } from "@ionic/react";
+import Toolbar from "../components/Toolbar";
+import TouristNumberContent from "../components/touristNumber/TouristNumberContent";
+import TouristNumberTitle from "../components/touristNumber/TouristNumberTitle";
+
 const TouristNumber: React.FC = () => {
-  return <h1> hola</h1>;
+  return (
+    <IonPage>
+      <IonHeader color="primary">
+        <Toolbar />
+      </IonHeader>
+      <IonContent fullscreen>
+        <TouristNumberTitle />
+        <TouristNumberContent />
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default TouristNumber;
